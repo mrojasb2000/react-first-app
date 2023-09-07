f703c484464c15910e6923009a1cab19eab3667f
maalroja <maalroja@bancolombia.com.co>
2023-09-07 13:50:06 -0300

    chore: add use state is following

--- components/twitter-card.jsx
+++ components/twitter-card.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react'
 import '../components/twitter-card.css'
 
 export function TwitterCard ({ formatElement, formatUserName, username, user, avatar }) {
+
+  const [isFollowing, setIsFollowing] = useState(false)
+
+  const text = isFollowing ?  'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
   const imageSrc = `https://api.dicebear.com/7.x/adventurer/svg?seed=${avatar}`
-  
+
+  const handlerClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
   return (
         <article className='followCard'>
             <header className='tw-followCard-header'>
@@ -15,8 +26,8 @@ export function TwitterCard ({ formatElement, formatUserName, username, user, av
             </header>
 
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handlerClick}>
+                    {text}
                 </button>
             </aside>
         </article>
--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { TwitterCard } from "../components/twitter-card";
 export function App () {
     const formatUserName = (username) => `@${username}`
     const formatElement = <span>Aprendiendo React</span>
-    return (<div className="App">
-     <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='Bandit' />
-     <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='bandit' />
-     <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='a' />
-     <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='b' />
-     <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='c' />
-    </div>)
+    return (
+        <div className="App">
+        <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='Bandit' />
+        <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='bandit' />
+        <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='a' />
+        <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='b' />
+        <TwitterCard formatElement={formatElement} formatUserName={formatUserName} user='Mauricio Rojas' username='mavrodev' avatar='c' />
+        </div>
+    )
 }
